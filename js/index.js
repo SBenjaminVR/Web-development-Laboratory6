4c8bfd51279d58a547f65739944ca30fee893629
Benjamín Valdez <benjamin.vr.1998@gmail.com>
2019-10-13 20:06:59 -0500

    Added next and previous buttons

--- js/index.js
+++ js/index.js
@@ -1,48 +1,73 @@
-function watchForm() {
-    $('#videoForm').on('submit', function (event) {
-        event.preventDefault();
+let token = '';
+let next = '';
+let previous = '';
+let pageNumber = 1;
 
-        $('#youtubeVideo').empty();
+function retrieveResults() {
+    let search = $("#video").val()
+    let APIkey = "AIzaSyBYjk7OZSZUpWkariquSuMuA__XtiemCsQ";
+    let APIurl = "https://www.googleapis.com/youtube/v3/search";
 
-        let search = $("#video").val()
-        let APIkey = "AIzaSyBYjk7OZSZUpWkariquSuMuA__XtiemCsQ";
-        let APIurl ="https://www.googleapis.com/youtube/v3/search";
+    $('#youtubeResults').empty();
+    $('#youtubeResults').append(`<ul id="youtubeVideo"></ul>`);
 
-        $.ajax({
-            url: APIurl,
-            method: "GET",
-            data: {
-                'key': APIkey,
-                'part': 'id, snippet',
-                'q': search,
-                'maxResults': '10',
-                'relevanceLanguage': 'en',
-                'type': 'video'
-            },
-            dataType: "json",
-            success: function (responseJSON) {
-                console.log(responseJSON);
-                for (let i = 0; i < responseJSON.items.length; i++) {
-                    $('#youtubeVideo').append(`<div class="ytVideo"><a target="_blank" href="https://www.youtube.com/watch?v=${responseJSON.items[i].id.videoId}"><li>${responseJSON.items[i].snippet.title}"</li><img src="${responseJSON.items[i].snippet.thumbnails.medium.url}"></a></div>`)
-                }
-                $('#youtubeResults').append(`<input type="submit" value="Get more results"/>`);
-            },
-            error: function (err) { 
+    $.ajax({
+        url: APIurl,
+        method: "GET",
+        data: {
+            'key': APIkey,
+            'part': 'id, snippet',
+            'q': search,
+            'maxResults': '10',
+            'relevanceLanguage': 'en',
+            'type': 'video',
+            'pageToken': token
+        },
+        dataType: "json",
+        success: function (responseJSON) {
+            console.log(responseJSON);
+            next = responseJSON.nextPageToken;
+            previous = responseJSON.prevPageToken;
 
+            for (let i = 0; i < responseJSON.items.length; i++) {
+                $('#youtubeVideo').append(`<div class="ytVideo"><a target="_blank" href="https://www.youtube.com/watch?v=${responseJSON.items[i].id.videoId}"><li>${responseJSON.items[i].snippet.title}"</li><img src="${responseJSON.items[i].snippet.thumbnails.medium.url}"></a></div>`)
             }
-        })
-    });
+                if (pageNumber === 1) {
+                    $('#youtubeResults').append(`<input type="submit" id="nextButton" value="Next"/>`);
+                }
+                else {
+                    $('#youtubeResults').append(`<input type="submit" id="previousButton" value="Previous"/><input type="submit" id="nextButton" value="Next"/>`);
+                }
+            firstSearch = false;
+        },
+        error: function (err) {
 
-    // Event delegation
-    /*
-    $('#youtubeVideo').on('click', '.ytVideo', function (event) {
-        event.preventDefault();
-        let youtubeURL = "https://www.youtube.com/watch?v=" + $(this).val();
-        console.log($(this));
-        console.log(youtubeURL);
-        //window.open(youtubeURL, '_blank');
+        }
     })
-    */
 }
 
-watchForm();
+function search() {
+    $('#videoForm').on('submit', function (event) {
+        event.preventDefault();
+        pageNumber = 1;
+        token = "";
+        retrieveResults();
+
+    });
+
+    $('#youtubeResults').on('click', '#previousButton', function (event) {
+        event.preventDefault();
+        token = previous;
+        pageNumber--;
+        retrieveResults();
+    });
+
+    $('#youtubeResults').on('click', '#nextButton', function (event) {
+        event.preventDefault();
+        token = next;
+        pageNumber++;
+        retrieveResults();
+    });
+}
+
+search();
